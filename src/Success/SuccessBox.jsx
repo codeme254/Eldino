8c56612b0b78522a148e5dfac52be38026e7f9e7
codeme254 <otwomadennis365@gmail.com>
2022-05-19 14:44:52 +0300

    created a build

--- src/Success/SuccessBox.jsx
+++ src/Success/SuccessBox.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const SuccessBox = ({ count, title }) => {
+const SuccessBox = ({ count, title, target }) => {
+  const counterRef = useRef(null);
+
+  // useEffect(function updateCounter() {
+  //   const targetNumber = +counterRef.current.dataset.target;
+  //   const initialNumber = +counterRef.current.innerText;
+  //   const incPerCount = targetNumber / 200;
+  //   if(initialNumber < targetNumber){
+  //     counterRef.current.innerText = Math.ceil(initialNumber + incPerCount)
+  //     setTimeout(updateCounter, 40)
+  //   }
+  // })
+
   return (
     <div className="success__box">
-      <h5 className="success__box--count">{count.toLocaleString()}</h5>
+      <h5 ref={counterRef} className="success__box--count" data-target={target}>
+        {count.toLocaleString()}
+      </h5>
       <p className="success__box--title">{title}</p>
     </div>
   );
--- src/Success/SuccessSection.jsx
+++ src/Success/SuccessSection.jsx
@@ -4,10 +4,10 @@ import SuccessBox from "./SuccessBox";
 const SuccessSection = () => {
   return (
     <section className="success-section">
-      <SuccessBox count={3000} title="rooms available" />
-      <SuccessBox count={600} title="nurses at work" />
-      <SuccessBox count={17000} title="elders attended to" />
-      <SuccessBox count={9000} title="happy customers" />
+      <SuccessBox target={3000} count={3000} title="rooms available" />
+      <SuccessBox target={600} count={600} title="nurses at work" />
+      <SuccessBox target={17000} count={17000} title="elders attended to" />
+      <SuccessBox target={9000}count={9000} title="happy customers" />
     </section>
   );
 };
