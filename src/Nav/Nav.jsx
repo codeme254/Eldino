e44c77bfa528358ebee4cd87bbe6845bd3a3582d
codeme254 <otwomadennis365@gmail.com>
2022-05-23 17:15:02 +0300

    started adding media queries

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,69 +1,79 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   Twitter,
   FacebookOutline,
   LinkedinOutlined,
+  MenuIcon
 } from "../Assets/Icons/Icons";
 
 const Nav = () => {
+  const navRef = useRef(null);
+  const handleNav = () => {
+    navRef.current.classList.toggle('header__nav--active')
+  }
   return (
     <header className="header">
       <h3 className="header__logo">
         Eldino<span>...</span>
       </h3>
-      <ol className="header__list">
-        <li className="header__list--item">
-          <a href="/" className="header__item--link">
-            home
-          </a>
-        </li>
-        <li className="header__list--item">
-          <a href="/" className="header__item--link">
-            about
-          </a>
-        </li>
-        <li className="header__list--item">
-          <a href="/" className="header__item--link">
-            success
-          </a>
-        </li>
-        <li className="header__list--item">
-          <a href="/" className="header__item--link">
-            services
-          </a>
-        </li>
-        <li className="header__list--item">
-          <a href="/" className="header__item--link">
-            goals
-          </a>
-        </li>
-        <li className="header__list--item">
-          <a href="/" className="header__item--link">
-            testimonies
-          </a>
-        </li>
-        <li className="header__list--item">
-          <a href="/" className="header__item--link">
-            opportunities
-          </a>
-        </li>
-        <li className="header__list--item">
-          <a href="/" className="header__item--link">
-            news & updates
-          </a>
-        </li>
-        <li className="header__list--icons-item">
-          <a href="/" className="header__link-icon">
-            <FacebookOutline />
-          </a>
-          <a href="/" className="header__link-icon">
-            <Twitter />
-          </a>
-          <a href="/" className="header__link-icon">
-            <LinkedinOutlined />
-          </a>
-        </li>
-      </ol>
+      <nav className="header__nav" ref={navRef}>
+        <ol className="header__list">
+          <li className="header__list--item">
+            <a href="/" className="header__item--link">
+              home
+            </a>
+          </li>
+          <li className="header__list--item">
+            <a href="/" className="header__item--link">
+              about
+            </a>
+          </li>
+          <li className="header__list--item">
+            <a href="/" className="header__item--link">
+              success
+            </a>
+          </li>
+          <li className="header__list--item">
+            <a href="/" className="header__item--link">
+              services
+            </a>
+          </li>
+          <li className="header__list--item">
+            <a href="/" className="header__item--link">
+              goals
+            </a>
+          </li>
+          <li className="header__list--item">
+            <a href="/" className="header__item--link">
+              testimonies
+            </a>
+          </li>
+          <li className="header__list--item">
+            <a href="/" className="header__item--link">
+              opportunities
+            </a>
+          </li>
+          <li className="header__list--item">
+            <a href="/" className="header__item--link">
+              news & updates
+            </a>
+          </li>
+          <li className="header__list--icons-item">
+            <a href="/" className="header__link-icon">
+              <FacebookOutline />
+            </a>
+            <a href="/" className="header__link-icon">
+              <Twitter />
+            </a>
+            <a href="/" className="header__link-icon">
+              <LinkedinOutlined />
+            </a>
+          </li>
+        </ol>
+      </nav>
+      <button className="header__button" onClick={handleNav}>
+        <MenuIcon />
+      </button>
     </header>
   );
 };
